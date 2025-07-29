4985994f2b6467249f2bc2d58294a08d007a4a7e
Elaina Wittmer <elaina.n.wittmer@gmail.com>
2025-07-29 10:51:15 -0500

    Removing specific dates, next 10 events w/i 60 days shown

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # siebelschool-calendar
+
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -30,28 +30,8 @@ export default function App() {
   if (!post) return (
     <div>
       <div class="note-container">
-        <div id="today" class="note-column">
-        <h2>Today</h2>
-          <div class="loading">
-              <l-dot-spinner
-                size="40"
-                speed="0.9" 
-                color="black" 
-              ></l-dot-spinner>
-            </div>
-        </div>
-        <div id="tomorrow" class="note-column">
-        <h2>Tomorrow</h2>
-          <div class="loading">
-            <l-dot-spinner
-              size="40"
-              speed="0.9" 
-              color="black" 
-            ></l-dot-spinner>
-          </div>
-        </div>
         <div id="upcoming" class="note-column">
-        <h2>Upcoming</h2>
+        <h2>Events</h2>
           <div class="loading">
             <l-dot-spinner
               size="40"
@@ -74,50 +54,14 @@ export default function App() {
     return month + '/' + day + '/' + year
   }
 
-  const today = dateFormat(0);
-  const tomorrow = dateFormat(1);
-  const filteredToday = post.events.filter(item => item.startDate._text == today);
-  const filteredTomorrow = post.events.filter(item => item.startDate._text == tomorrow);
-  const filteredWeek = post.events.filter(item => Date.parse(item.startDate._text) > Date.parse(tomorrow));
-  console.log(filteredTomorrow.length)
+  const thisMonth = dateFormat(60);
+  const filteredWeek = post.events.filter(item => Date.parse(item.startDate._text) < Date.parse(thisMonth));
 
   return (
     <div>
       <div class="note-container">
-        <div id="today" class="note-column">
-        <h2>Today</h2>
-        {filteredToday.length === 0 ? 
-          <div class="note">
-            <p>No remaining events today!</p>
-          </div>
-        :
-          filteredToday.map((event) => (
-              <Event key={event.eventId._text}
-              title={event.title._text}
-              startTime={event.startTime._text}
-              startDate={event.startDate._text}
-              location={event.location._text}
-              virtualEvent={event.virtualEvent._text}/>
-            ))}
-        </div>
-        <div id="tomorrow" class="note-column">
-        <h2>Tomorrow</h2>
-        {filteredTomorrow.length === 0 ? 
-          <div class="note">
-            <p>No events tomorrow!</p>
-          </div>
-        :
-            filteredTomorrow.map((event) => (
-              <Event key={event.eventId._text}
-              title={event.title._text}
-              startTime={event.startTime._text}
-              startDate={event.startDate._text}
-              location={event.location._text}
-              virtualEvent={event.virtualEvent._text}/>
-            ))}
-        </div>
         <div id="upcoming" class="note-column">
-        <h2>Upcoming</h2>
+        <h2>Events</h2>
         {filteredWeek.length === 0 ? 
         <div class="note">
           <p>No upcoming events!</p>
